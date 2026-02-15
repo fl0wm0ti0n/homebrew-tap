728d7267f4376de173d0544d2ef8ac42f0ac06af
Florian Gabriel <flow@p0cki.net>
2026-02-15 20:02:15 +0100

    chore: update its-magic formulas for 0.1.2-5

--- Formula/its-magic-beta.rb
+++ Formula/its-magic-beta.rb
@@ -2,9 +2,9 @@ class ItsMagicBeta < Formula
   desc "its-magic - AI dev team workflow (beta)"
   homepage "https://github.com/USER/its-magic"
   # UPDATE: url and sha256 after each beta release
-  url "https://github.com/fl0wm0ti0n/gsd_cursor/archive/refs/tags/v0.1.2-4.tar.gz"
-  sha256 "2d88b74aafffb8e6282559b0038889a4c9179d5db2a12bffd7822b082425aa09"
-  version "0.1.2-4"
+  url "https://github.com/fl0wm0ti0n/gsd_cursor/archive/refs/tags/v0.1.2-5.tar.gz"
+  sha256 "d83d466ea9ad8d43740edefcc037e649ef88e7ef52d2954ca042a32d7d2cb938"
+  version "0.1.2-5"
   license "MIT"
 
   depends_on "node"
@@ -20,6 +20,7 @@ class ItsMagicBeta < Formula
     system "#{bin}/its-magic", "--help"
   end
 end
+
 
 
 
