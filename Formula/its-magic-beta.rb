fa0da2b0d7a3f0e640061adffa422b3e2f24a079
Florian Gabriel <flow@p0cki.net>
2026-02-17 22:52:59 +0100

    chore: update its-magic formulas for 0.1.2-12

--- Formula/its-magic-beta.rb
+++ Formula/its-magic-beta.rb
@@ -2,9 +2,9 @@ class ItsMagicBeta < Formula
   desc "its-magic - AI dev team workflow (beta)"
   homepage "https://github.com/USER/its-magic"
   # UPDATE: url and sha256 after each beta release
-  url "https://github.com/fl0wm0ti0n/gsd_cursor/archive/refs/tags/v0.1.2-11.tar.gz"
-  sha256 "da04cc3d9ce8765d571a171334d19f52b4c071cb57e46bca48ac84b640300d05"
-  version "0.1.2-11"
+  url "https://github.com/fl0wm0ti0n/gsd_cursor/archive/refs/tags/v0.1.2-12.tar.gz"
+  sha256 "1f868f5a333a34e3dd818506b51bc1365ecca440f778d11a2bea82520475bca0"
+  version "0.1.2-12"
   license "MIT"
 
   conflicts_with "its-magic", because: "beta and stable share the same binary"
@@ -39,6 +39,7 @@ class ItsMagicBeta < Formula
     system "#{bin}/its-magic", "--target", testpath, "--mode", "missing"
   end
 end
+
 
 
 
