fed98e7d34fd32c825541f8e76c85c3dfb330f7f
flow <flow@p0cki.net>
2026-09-11 22:54:21 +0200

    chore: update its-magic formulas for 0.1.3-9

--- Formula/its-magic-beta.rb
+++ Formula/its-magic-beta.rb
@@ -2,9 +2,9 @@ class ItsMagicBeta < Formula
   desc "its-magic - AI dev team workflow (beta)"
   homepage "https://github.com/USER/its-magic"
   # UPDATE: url and sha256 after each beta release
-  url "https://github.com/fl0wm0ti0n/its-magic/archive/refs/tags/v0.1.3-8.tar.gz"
-  sha256 "6e87697e52b62daac6e0740c6ae06a8b3832a4ec6ca9f83a915e8edd3acde138"
-  version "0.1.3-8"
+  url "https://github.com/fl0wm0ti0n/its-magic/archive/refs/tags/v0.1.3-9.tar.gz"
+  sha256 "1fdc5c62bbd26709538882ff7ec7e78daa6f64c4426730f96e7def4d7b66fffd"
+  version "0.1.3-9"
   license "MIT"
 
   conflicts_with "its-magic", because: "beta and stable share the same binary"
@@ -39,59 +39,4 @@ class ItsMagicBeta < Formula
     system "#{bin}/its-magic", "--target", testpath, "--mode", "missing"
   end
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
